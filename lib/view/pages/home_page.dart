8478ae0762f854cbaf9c105098a06949630c3104
hp <sultonovazizbek73@gmail.com>
2025-04-26 04:06:46 +0500

    chart

--- lib/view/pages/home_page.dart
+++ lib/view/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tracker/controller/provider/expanse_provider.dart';
 import 'package:tracker/controller/provider/fliter_provider.dart';
 import 'package:tracker/repository/models/expanse_model.dart';
+import 'package:tracker/view/widgets/stats_widget.dart';
 import 'package:uuid/uuid.dart';
 
 class HomePage extends ConsumerWidget {
@@ -61,38 +62,48 @@ class HomePage extends ConsumerWidget {
         ],
         title: Text("Expanse Tracker"),
       ),
-      body: expanses.isEmpty
-          ? const Center(
-              child: Text('No expanses yet..'),
-            )
-          : ListView.builder(
-              itemCount: filteredExpanses.length,
-              itemBuilder: (context, index) {
-                final expanse = filteredExpanses[index];
-                return ListTile(
-                  title: Text(expanse.title),
-                  subtitle: Text(
-                      "${expanse.amount} so'm  ${expanse.date.day}/${expanse.date.month}/${expanse.date.year}"),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            showExpanseDialog(context, ref,
-                                isEdit: true, expanse: expanse);
-                          },
-                          icon: Icon(Icons.edit)),
-                      IconButton(
-                          onPressed: () {
-                            ref
-                                .read(expanseProvider.notifier)
-                                .deleteExpanse(expanse.id);
-                          },
-                          icon: Icon(Icons.delete)),
-                    ],
-                  ),
-                );
-              }),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: StatsWidget(expanses: filteredExpanses)),
+          Divider(),
+          Expanded(
+            child: expanses.isEmpty
+                ? const Center(
+                    child: Text('No expanses yet..'),
+                  )
+                : ListView.builder(
+                    itemCount: filteredExpanses.length,
+                    itemBuilder: (context, index) {
+                      final expanse = filteredExpanses[index];
+                      return ListTile(
+                        title: Text(expanse.title),
+                        subtitle: Text(
+                            "${expanse.amount} so'm  ${expanse.date.day}/${expanse.date.month}/${expanse.date.year}"),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  showExpanseDialog(context, ref,
+                                      isEdit: true, expanse: expanse);
+                                },
+                                icon: Icon(Icons.edit)),
+                            IconButton(
+                                onPressed: () {
+                                  ref
+                                      .read(expanseProvider.notifier)
+                                      .deleteExpanse(expanse.id);
+                                },
+                                icon: Icon(Icons.delete)),
+                          ],
+                        ),
+                      );
+                    }),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => showExpanseDialog(
           context,
---
+++ lib/view/widgets/stats_widget.dart
@@ -0,0 +1,110 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:tracker/repository/models/expanse_model.dart';
+
+class StatsWidget extends StatelessWidget {
+  final List<ExpanseModel> expanses;
+  const StatsWidget({super.key, required this.expanses});
+
+  @override
+  Widget build(BuildContext context) {
+    final now = DateTime.now();
+    final Map<String, double> dailyTotals = {
+      "Mon": 0,
+      "Tue": 0,
+      "Wed": 0,
+      "Thu": 0,
+      "Fri": 0,
+      "Sat": 0,
+      "Sun": 0,
+    };
+
+    for (var e in expanses) {
+      if (e.date.isAfter(
+        now.subtract(
+          Duration(days: 7),
+        ),
+      )) {
+        final weekday = _weekdayLabel(e.date.weekday);
+        dailyTotals[weekday] = dailyTotals[weekday]! + e.amount;
+      }
+    }
+
+    return Padding(
+      padding:EdgeInsets.all(10), 
+      child:BarChart(
+        BarChartData(
+          alignment: BarChartAlignment.spaceAround,
+          titlesData: FlTitlesData(
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                getTitlesWidget: (value,meta){
+                  final days=['Mon','Tue','Wed','Thu','Fri','Sat','Sun'];
+                  return Text(days[value.toInt()]);
+                },
+                reservedSize: 30,
+
+              ),
+            ),
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: true,
+                reservedSize: 40,
+              ),
+            ),
+            rightTitles: AxisTitles(
+              sideTitles: SideTitles(
+                showTitles: false,
+              ),
+            ),
+            topTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: false,
+            ),
+            ),
+            
+          ),
+          barGroups: List.generate(7, (index){
+            final label=_weekdayLabel(index+1);
+            final value=dailyTotals[label]??0;
+            return BarChartGroupData(
+              x: index,
+              barRods: [
+                BarChartRodData(
+                  toY: value,
+                  color: Colors.purpleAccent,
+                  width: 16,
+                ),
+              ],
+            );
+          })
+        ),
+      ),
+    );
+  }
+
+
+
+  String _weekdayLabel(int weekday){
+    switch(weekday){
+      case 1:
+        return 'Mon';
+      case 2:
+        return 'Tue';
+      case 3:
+        return 'Wed';
+      case 4:
+        return 'Thu';
+      case 5:
+        return 'Fri';
+      case 6:
+        return 'Sat';
+      case 7:
+        return 'Sun';
+      default:
+        return '';
+
+    }
+  }
+}
